a3bea99bc2466ce93bf6f646e38213b87e1429f8
aduna-kebeda <119304208+aduna-kebeda@users.noreply.github.com>
2023-11-04 12:54:05 +0300

    Time: 4 ms (41.56%), Space: 7.5 MB (15.03%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> result(n, vector<int>(n, 0));
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        
+        matrix = result;
+    }
+};
