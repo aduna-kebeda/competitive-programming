f6b36e049fe472bc1b6f75675df1b7b7e2ba1c15
aduna-kebeda <119304208+aduna-kebeda@users.noreply.github.com>
2023-11-12 22:16:01 +0300

    Time: 23 ms (14.26%), Space: 13.2 MB (61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,21 +1,11 @@
 class Solution(object):
     def sortColors(self, nums):
-        c0 = nums.count(0)
-        c1 = nums.count(1)
-        c2 = nums.count(2)
-        
-        index = 0
-        
-        for i in range(c0):
-            nums[index] = 0
-            index += 1
-            
-        for i in range(c1):
-            nums[index] = 1
-            index += 1
-            
-        for i in range(c2):
-            nums[index] = 2
-            index += 1
-            
+        for i in range(len(nums)-1):
+            minIndex=i
+            for j in range(i+1, len(nums)):
+                if nums[j]<nums[minIndex]:
+                    minIndex=j
+            temp=nums[i]
+            nums[i]=nums[minIndex]
+            nums[minIndex]=temp
         return nums
