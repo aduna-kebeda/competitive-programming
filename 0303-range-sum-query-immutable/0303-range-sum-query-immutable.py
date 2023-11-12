5e87ffaa12809176d1c4a1fcfbb7a6a496d8c732
aduna-kebeda <119304208+aduna-kebeda@users.noreply.github.com>
2023-11-12 08:24:16 +0300

    Time: 53 ms (77.2%), Space: 16.9 MB (66.01%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -0,0 +1,8 @@
+class NumArray(object):
+    def __init__(self, nums):
+        self.prefix_sum = [0]  
+        for num in nums:
+            self.prefix_sum.append(self.prefix_sum[-1] + num)  
+
+    def sumRange(self, left, right):
+        return self.prefix_sum[right + 1] - self.prefix_sum[left]
