9021103d7ce15308b0b1d652ec2a51f3acc25d2c
aduna-kebeda <119304208+aduna-kebeda@users.noreply.github.com>
2024-01-06 21:40:23 +0300

    Added solution for Sort Colors

--- week3/sort-colors.py
+++ week3/sort-colors.py
@@ -1,11 +1,14 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
+        p=0
         for i in range(len(nums)):
-            j=i
-            while j>0:
-                if nums[j]<nums[j-1]:
-                    nums[j], nums[j-1]=nums[j-1], nums[j]
-                else:
-                    break
-                j-=1
+            if nums[i]==0 :
+                nums[i], nums[p]=nums[p], nums[i]
+                p+=1
+        
+       
+        for i in range(p, len(nums)):
+            if nums[i]==1:
+                nums[i], nums[p]=nums[p], nums[i]
+                p+=1
         return nums
