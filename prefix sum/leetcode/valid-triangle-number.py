bec31b94cc9c201144b869a285943d4120651b7d
aduna-kebeda <119304208+aduna-kebeda@users.noreply.github.com>
2024-02-22 09:41:08 +0300

    Add solution for Valid Triangle Number

---
+++ prefix sum/leetcode/valid-triangle-number.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        cnt = 0
+        for r in range(2, len(nums)):
+            l = 0
+            j = r - 1
+            while l < j:
+                if nums[l] + nums[j] > nums[r]:
+                    cnt += j - l
+                    j -= 1
+                else:
+                    l += 1
+        return cnt
