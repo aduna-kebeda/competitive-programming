8e84274d67d7ea4247720a8b3b394f52a8ab0e6e
aduna-kebeda <119304208+aduna-kebeda@users.noreply.github.com>
2024-03-07 23:44:12 +0300

    Add solution for Maximum Width of Binary Tree

---
+++ prefix sum/leetcode/maximum-width-of-binary-tree.py
@@ -0,0 +1,29 @@
+from collections import Counter
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        dic = defaultdict(list)
+        
+        def rec(node, depth, position):
+            if not node:
+                return
+
+            dic[depth].append(position)
+            rec(node.left, depth + 1, 2 * position)
+            rec(node.right, depth + 1, 2 * position + 1)
+
+        rec(root, 0, 0)
+        maxi=0
+        for key, val in dic.items():
+            maxi=max(maxi, max(val)- min(val))
+        return maxi + 1
+
+    
